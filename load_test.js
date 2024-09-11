31105bb1b6fbcf468ac4d6eea667d227d49a5b05
Jeremiah Tabb <jollyjerr@gmail.com>
2024-09-10 20:09:39 -0600

    refactor: better example

--- load_test.js
+++ load_test.js
@@ -1,20 +1,23 @@
 import { check } from "k6";
 import http from "k6/http";
 
+const STRESS_FACTOR = 2;
+// const STRESS_FACTOR = 25;
+// const STRESS_FACTOR = 250;
+
 export const options = {
   thresholds: {
     http_req_failed: ["rate<0.01"], // http errors should be less than 1%
     http_req_duration: ["p(99)<1000"], // 99% of requests should be below 1s
   },
-  stages: [{ duration: "30s", target: 10 }],
+  stages: [{ duration: "10s", target: 420 }],
 };
 
 export default function () {
-  const url = "http://localhost:8080/test/100";
+  const url = `http://localhost:8080/test/${STRESS_FACTOR}`;
 
-  const res = http.get(url);
+  const res = http.get(url, { timeout: "5s" });
 
-  // check that response is 200
   check(res, {
     "response code was 200": (res) => res.status == 200,
   });
